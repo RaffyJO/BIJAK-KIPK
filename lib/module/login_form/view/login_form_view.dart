491e948839ae106c87ccadd9e8bbb30178395138
ManggalaKZ <55419121+ManggalaKZ@users.noreply.github.com>
2023-08-29 18:05:45 +0700

    login
    baru

--- lib/module/login_form/view/login_form_view.dart
+++ lib/module/login_form/view/login_form_view.dart
@@ -61,17 +61,16 @@ class LoginFormView extends StatefulWidget {
                       width: 150,
                       height: 50,
                       child: QButton(
-                        label: "LogIn",
-                        color: Color(0xFF9B51E0),
-                        onPressed: () => SignUpFormView(),
-                        // onPressed: () {
-                        //   Navigator.push(
-                        //     context,
-                        //     MaterialPageRoute(
-                        //         builder: (context) => SignUpFormView()),
-                        //   );
-                        // }
-                      ),
+                          label: "LogIn",
+                          color: Color(0xFF9B51E0),
+                          // onPressed: () => SignUpFormView(),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SignUpFormView()),
+                            );
+                          }),
                     ),
                     Container(
                       width: 150,
